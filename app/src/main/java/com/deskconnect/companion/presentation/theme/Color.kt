6346cff6dcf388127fd9213d4efe4df6c5d33343
subodhcoc222-dev <subodhcoc222@gmail.com>
2026-09-08 16:20:07 +0530

    Create Color.kt

---
+++ app/src/main/java/com/deskconnect/companion/presentation/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.deskconnect.companion.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Background & Surfaces
+val SlateDark = Color(0xFF0F172A)
+val SlateSurface = Color(0xFF1E293B)
+val SlateSurfaceLight = Color(0xFF334155)
+val SlateBorder = Color(0xFF475569)
+
+// Functional & State Accents
+val EmeraldPresent = Color(0xFF10B981) // Present Time / Live Active
+val AmberBreak = Color(0xFFF59E0B)     // Break Time / Paused / Warning
+val CrimsonAbsent = Color(0xFFEF4444)  // Absent Time / Alert / Violation
+val CyanAccent = Color(0xFF06B6D4)     // Primary Action / Telemetry
+val TextWhite = Color(0xFFF8FAFC)
+val TextMuted = Color(0xFF94A3B8)
